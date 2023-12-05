18652ead16854b6569bc4eefea196e3c37f14690
acoto-gu <arleycoto94@gmail.com>
2023-12-05 11:46:53 +0100

    Change completely my app

--- src/server.c
+++ src/server.c
@@ -6,97 +6,90 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 10:00:03 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/11/27 13:17:48 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/12/05 11:13:34 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-static t_rx_state	*get_rx_state(void)
+static void	error_server(t_rx_state *rx)
 {
-	static t_rx_state	rx;
-
-	return (&rx);
+	ft_printf("server: unexpected error.\n");
+	kill(rx->pid_client, SIGUSR2);
+	if (rx->msg)
+		free (rx->msg);
+	exit(EXIT_FAILURE);
 }
 
-static void	rx_len_hanlder(int signum, siginfo_t *siginfo, void *ucontext)
+static int	rx_msg(t_rx_state *rx, int signum)
 {
-	t_rx_state			*rx;
-
-	(void) ucontext;
-	rx = get_rx_state();
-	if (rx->pid_client == 0)
-		rx->pid_client = siginfo->si_pid;
-	else if (rx->pid_client != 0 && rx->pid_client != siginfo->si_pid)
-		return ;
-	if (signum == SIGUSR2)
-		rx->msg_len = (0x80000000 >> rx->bit_count) | rx->msg_len;
-	rx->bit_count++;
 	if (rx->bit_count < 32)
-		kill(siginfo->si_pid, SIGUSR1);
+	{
+		if (signum == SIGUSR2)
+			rx->msg_len |= (0x80000000 >> rx->bit_count);
+		if (rx->bit_count == 31)
+		{
+			rx->msg = malloc(rx->msg_len + 1);
+			if (!rx->msg)
+				error_server(rx);
+			ft_bzero(rx->msg, rx->msg_len);
+			ft_printf("Rx %d bytes from id:%d\n", rx->msg_len, rx->pid_client);
+		}
+	}
 	else
 	{
-		rx->bit_count = 0;
-		rx->ready = 1;
+		if (signum == SIGUSR2)
+			rx->msg[(rx->bit_count - 32) / 8] |= 0x80
+				>> ((rx->bit_count - 32) % 8);
+		if (((rx->bit_count - 32) + 1) / 8 == rx->msg_len)
+			return (1);
 	}
+	rx->bit_count++;
+	return (0);
 }
 
-static void	rx_msg_hanlder(int signum, siginfo_t *siginfo, void *ucontext)
+static void	handler(int signum, siginfo_t *siginfo, void *ucontext)
 {
-	t_rx_state			*rx;
+	t_rx_state	*rx;
+	int			end;
 
 	(void) ucontext;
 	rx = get_rx_state();
+	if (!rx->pid_client)
+		rx->pid_client = siginfo->si_pid;
 	if (rx->pid_client != siginfo->si_pid)
-		return ;
-	if (signum == SIGUSR2)
-		rx->byte = (0x80 >> rx->bit_count) | rx->byte;
-	rx->bit_count++;
-	if (rx->bit_count == 8)
 	{
-		rx->msg[rx->byte_count] = rx->byte;
-		rx->bit_count = 0;
-		rx->byte = 0;
-		rx->byte_count++;
+		kill(siginfo->si_pid, SIGUSR2);
+		return ;
 	}
-	kill(siginfo->si_pid, SIGUSR1);
-}
-
-void	set_sigaction(struct sigaction *act,
-	void (*handler)(int, siginfo_t *, void *))
-{
-	sigemptyset(&(act->sa_mask));
-	act->sa_sigaction = handler;
-	act->sa_flags = SA_RESTART | SA_SIGINFO;
-	sigaction(SIGUSR1, act, NULL);
-	sigaction(SIGUSR2, act, NULL);
+	end = 0;
+	end = rx_msg(rx, signum);
+	if (end)
+	{
+		rx->msg[rx->msg_len] = 0;
+		ft_printf("%s\n", rx->msg);
+		if (rx->msg)
+			free (rx->msg);
+		kill (rx->pid_client, SIGUSR1);
+		ft_bzero(rx, sizeof(t_rx_state));
+	}
+	else if (kill(rx->pid_client, SIGUSR1) == -1)
+		error_server(rx);
 }
 
 int	main(void)
 {
 	struct sigaction	act;
-	t_rx_state			*rx;
 
 	ft_printf("Server PID: %d\n", getpid());
+	sigemptyset(&(act.sa_mask));
+	act.sa_sigaction = handler;
+	act.sa_flags = SA_RESTART | SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 	{
-		set_sigaction(&act, rx_len_hanlder);
-		rx = get_rx_state();
-		while (!rx->ready)
-			sleep(1);
-		rx->msg = malloc(sizeof(char) * (rx->msg_len + 1));
-		if (!rx->msg)
-			return (ft_printf("Fail allocating memory"), EXIT_FAILURE);
-		ft_printf("Rx %d bytes from PID: %d\n", rx->msg_len, rx->pid_client);
-		set_sigaction(&act, rx_msg_hanlder);
-		kill(rx->pid_client, SIGUSR1);
-		while (rx->byte_count < rx->msg_len)
-			sleep(1);
-		rx->msg[rx->byte_count] = 0;
-		ft_printf("%s\n", rx->msg);
-		kill(rx->pid_client, SIGUSR2);
-		free(rx->msg);
-		ft_bzero(rx, sizeof(t_rx_state));
+		pause();
 	}
-	return (0);
+	return (EXIT_SUCCESS);
 }
